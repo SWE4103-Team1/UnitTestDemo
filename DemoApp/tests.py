075419332dc2848d37d07291a8b927142643875f
EthanG78 <ethangarnier78@gmail.com>
2021-09-27 23:41:52 -0300

    Added mock unit tests for demo

--- DemoApp/tests.py
+++ DemoApp/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
 
-# Create your tests here.
+# Unit test class inherits from django.test.TestCase
+# which inherits from unittest.TestCase
+class DemoAppUnitTests(TestCase):
+	def test_true_is_true(self):
+		isTrue = True
+		self.assertTrue(isTrue)
+
+	def test_false_is_false(self):
+		isFalse = False
+		self.assertFalse(isFalse)
